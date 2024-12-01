0cbb50e18d733eac28d2e723190db038a7a83fb2
Lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2024-12-01 15:28:03 +0000

    FINALLY
    The final commit-down

--- build/build-ml.sh
+++ build/build-ml.sh
@@ -13,9 +13,9 @@ libusb_version='v1.0.27'
 libedgetpu_version='v0.1.9'
 tensorflow_version='v2.16.1'
 
-install_prefix='/build/install-prefix'
+install_prefix='/deps'
 
-export PKG_CONFIG_PATH="$install_prefix/lib/pkgconfig"
+export PKG_CONFIG_PATH="/deps/lib/pkgconfig"
 
 # # If Bazel isn't installed, go through the entire process to download/bootstrap it w/ Bazelisk :/
 # if ! command -v bazel; then
@@ -80,12 +80,10 @@ __tflite() {
 
 	popd #build
 
-	mkdir -p $install_prefix/include $install_prefix/lib
- 	find . -name 'tensorflow/lite/*.h' -exec cp --parents '{}' $install_prefix/include \;
-  	find . -name 'build/*.a' -exec cp '{}' $install_prefix/lib \;
-  	find . -name 'build/*.a' -exec cp '{}' /usr/local/lib \;
-   	find . -name 'build/*.so' -exec cp '{}' $install_prefix/lib \;
-   	find . -name 'build/*.so' -exec cp '{}' /usr/local/lib \;
+	mkdir -p /deps/include /deps/lib
+ 	find tensorflow/lite -type f -name '*.h' -exec cp --parents '{}' /deps/include \;
+  	find build -type f -name '*.a' -exec cp '{}' /deps/lib \;
+   	find build -type f -name '*.so' -exec cp '{}' /deps/lib \;
   
 	popd #tensorflow
 }
@@ -101,7 +99,7 @@ __libusb() {
 	# -fPIC: Position Independent Code (tells the linker to not use specific locations)
 	# --enable-{shared,static}: Enables building the library's statically- and dynamically-linked versions
 	# --disable-udev: 
-	CFLAGS="-fPIC" ./configure --enable-static --enable-shared --disable-udev --prefix="/build/install-prefix"
+	CFLAGS="-fPIC" ./configure --enable-static --enable-shared --disable-udev --prefix="/deps"
 
 	make
 	make install
@@ -120,7 +118,7 @@ __abseil_is_bs() {
 	mkdir -p build
 	pushd build
 
-	cmake -DCMAKE_SHARED_LIBRARY=TRUE -DCMAKE_STATIC_LIBRARY=TRUE -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/build/install-prefix" ../
+	cmake -DCMAKE_SHARED_LIBRARY=TRUE -DCMAKE_STATIC_LIBRARY=TRUE -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/deps" ../
 	make
  	make install
 
@@ -134,14 +132,14 @@ __libedgetpu() {
 	git checkout $libedgetpu_version
 
 	# Build it
-	export CFLAGS="-L/build/install-prefix/lib -I/build/install-prefix/include"
-	export CXXFLAGS="-L/build/install-prefix/lib -I/build/install-prefix/include"
- 	export LDFLAGS="-L/build/install-prefix/lib -L/usr/lib/x86_64-linux-gnu"
-  	TFROOT=/build/tensorflow/ LD_LIBRARY_PATH=/build/install-prefix/lib/ make -f makefile_build/Makefile libedgetpu
+	export CFLAGS="-L/deps/lib -I/deps/include"
+	export CXXFLAGS="-L/deps/lib -I/deps/include"
+ 	export LDFLAGS="-L/deps/lib -L/usr/lib/x86_64-linux-gnu"
+  	TFROOT=/build/tensorflow/ LD_LIBRARY_PATH=/deps/lib/ make -f makefile_build/Makefile libedgetpu
 
 	pushd out
-	mv direct/*/libedgetpu.so.1.0 direct/libedgetpu.so
-	mv throttled/*/libedgetpu.so.1.0 throttled/libedgetpu.so
+	cp direct/*/libedgetpu.so.1.0 /deps/lib
+	#mv throttled/*/libedgetpu.so.1.0 throttled/libedgetpu.so
 	popd #out
 
 	popd #libedgetpu
