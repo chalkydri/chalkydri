27ef22b0061099554449a7ef112bcd54f6312845
Lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2024-12-01 15:39:21 +0000

    Update tfledge build to work under Docker image

--- crates/chalkydri-tfledge/build.rs
+++ crates/chalkydri-tfledge/build.rs
@@ -10,8 +10,7 @@ fn main() {
     #[cfg(feature = "__bindgen")]
     {
         let mut b = bindgen::builder()
-            .clang_arg("-I../../third_party/libedgetpu/tflite/public")
-            .clang_arg("-I../../third_party/tensorflow")
+            .clang_arg("-I/deps/include")
             .rustified_enum(".*")
             .use_core();
         for h in [
@@ -28,23 +27,7 @@ fn main() {
         b.generate().unwrap().write_to_file("src/gen.rs").unwrap();
     }
 
-    #[cfg(feature = "direct")]
-    println!(
-        "cargo:rustc-link-search={}/../../third_party/libedgetpu/out/direct",
-        env!("CARGO_MANIFEST_DIR")
-    );
-
-    #[cfg(feature = "throttled")]
-    println!(
-        "cargo:rustc-link-search={}/../../third_party/libedgetpu/out/throttled",
-        env!("CARGO_MANIFEST_DIR")
-    );
-
+    println!("cargo:rustc-link-search=/deps/lib");
     println!("cargo:rustc-link-lib=edgetpu");
-
-    println!(
-        "cargo:rustc-link-search={}/../../third_party/tensorflow/build",
-        env!("CARGO_MANIFEST_DIR")
-    );
     println!("cargo:rustc-link-lib=tensorflowlite_c");
 }
