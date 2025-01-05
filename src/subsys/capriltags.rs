fe8b36a99f42088117620d36a0df232e6ceb017c
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-01-05 17:05:44 -0500

    finalized libcamera integration, restructured capriltags subsystem

--- src/subsys/capriltags.rs
+++ src/subsys/capriltags.rs
@@ -9,17 +9,55 @@
 // <https://www.chiefdelphi.com/t/frc-blog-technology-updates-past-present-future-and-beyond-apriltags-and-new-radio/440931>
 // According to this post on CD, we're doing the 36h11 tag family now.
 
-use apriltag::Detector;
+use actix::{Actor, Addr, Handler, SyncArbiter, SyncContext};
+use apriltag::{Detector, Family, Image, TagParams};
+use apriltag_image::image::{DynamicImage, RgbImage};
+use apriltag_image::prelude::*;
 
-use crate::Subsystem;
+use crate::{ProcessFrame, Subsystem};
 
 pub struct CApriltagsDetector {
     det: apriltag::Detector,
 }
-impl Subsystem<'_, ()> for CApriltagsDetector {
+impl Subsystem<'_> for CApriltagsDetector {
     type Processor = Self;
     type Config = ();
+    type Output = ();
+    type Error = Box<dyn std::error::Error + Send>;
 
-    async fn init() -> Result<Self, Box<dyn std::error::Error>> {
+    async fn init(_cfg: Self::Config) -> Result<Addr<Self>, Self::Error> {
+        Ok(SyncArbiter::start(1, move || {
+            let det = Detector::builder()
+                .add_family_bits(Family::tag_16h5(), 3)
+                .build()
+                .unwrap();
+
+            Self { det }
+        }))
+    }
+}
+
+impl Actor for CApriltagsDetector {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<ProcessFrame<(), Box<dyn std::error::Error + Send>>> for CApriltagsDetector {
+    type Result = Result<(), Box<dyn std::error::Error + Send>>;
+
+    fn handle(
+        &mut self,
+        msg: ProcessFrame<(), Box<dyn std::error::Error + Send>>,
+        _ctx: &mut Self::Context,
+    ) -> Self::Result {
+        let img_rgb =
+            DynamicImage::ImageRgb8(RgbImage::from_vec(1920, 1080, msg.buf.to_vec()).unwrap());
+        let img_gray = img_rgb.grayscale();
+        let buf = img_gray.as_luma8().unwrap();
+        let img = Image::from_image_buffer(buf);
+
+        let dets = self.det.detect(&img);
+        dets.first().unwrap();
+
+        Ok(())
     }
 }
