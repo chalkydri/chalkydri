24f89c4abbdc41c933d58af52b50ac40c9549501
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-01-19 17:31:54 -0500

    Add simple NetworkTables server script

---
+++ tools/ntserver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S uv run --project tools
+'''
+Run a NetworkTables server
+'''
+
+from ntcore import NetworkTableInstance as NTInstance
+import time
+import logging
+
+def main():
+    inst = NTInstance.getDefault()
+    inst.configPythonLogging(min=NTInstance.LogLevel.kLogDebug4)
+
+    inst.startServer()
+
+    while True:
+        topics = inst.getTopics('/')
+        if len(topics) > 0:
+            print(topics)
+
+if __name__ == '__main__':
+    main()
