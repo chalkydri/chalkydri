59814c372fe8e2a630ebd4dcadd4ddecbcd61a5b
chofuu <144732960+chofuu@users.noreply.github.com>
2025-01-27 18:19:36 -0500

    update machine learning subsystem

--- src/subsys/ml/mod.rs
+++ src/subsys/ml/mod.rs
@@ -2,7 +2,7 @@
 //! Machine learning subsystem
 //!
 
-use crate::{ProcessFrame, Subsystem};
+use crate::{ProcessFrame, Subsystem, Cfg};
 use actix::prelude::*;
 use tfledge::{CoralDevice, Error, Input, Interpreter, Model, Output, Tensor};
 
@@ -10,17 +10,22 @@ use tfledge::{CoralDevice, Error, Input, Interpreter, Model, Output, Tensor};
 pub struct MlInterpreter {
     int: Interpreter,
 }
-impl Actor for MlInterpreter {
-    type Context = Context<Self>;
-}
-impl Handler<ProcessFrame<'_, Tensor<Output, f32>>> for MlInterpreter {
-    type Result = Result<Tensor<Output, f32>, Box<dyn std::error::Error>>;
+impl<'fr> Subsystem<'fr> for MlInterpreter {
+    async fn init() -> Result<Self, Self::Error> {
+        // Get the first available Coral device
+        let dev = tfledge::list_devices()
+            .next()
+            .expect("no Coral devices found"); 
 
-    fn handle(
-        &mut self,
-        msg: ProcessFrame<'_, Tensor<Output, f32>>,
-        ctx: &mut Self::Context,
-    ) -> Self::Result {
+        // Load TFLite model from the path in the config
+        let model = Model::from_file(Cfg.model_path.as_str()).unwrap();
+
+        let int = Interpreter::new(model, dev).unwrap();
+        
+        Self { int }
+    }
+
+    fn process(&mut self, buf: crate::subsystem::Buffer) -> Result<Self::Output, Self::Error> {
         // Set up the input tensors
 
         self.int.invoke().expect("failed to invoke the interpreter");
@@ -29,36 +34,6 @@ impl Handler<ProcessFrame<'_, Tensor<Output, f32>>> for MlInterpreter {
 
         Ok(self.int.output_tensor(0))
     }
-}
-
-pub struct MlSubsysCfg {
-    pub model_path: String,
-}
-
-/// Chalkydri's machine learning subsystem
-///
-/// This uses [::tfledge] to interact with Coral devices through TensorFlow Lite.
-pub struct MlSubsys {
-    dev: CoralDevice,
-}
-impl Subsystem<'_, Tensor<Output, f32>> for MlSubsys {
-    type Processor = MlInterpreter;
-    type Config = MlSubsysCfg;
-
-    async fn init() -> Result<Self, Box<dyn std::error::Error>> {
-        // Get the first available Coral device
-        let dev = tfledge::list_devices()
-            .next()
-            .expect("no Coral devices found");
-
-        Ok(Self { dev })
-    }
-    async fn run(self, cfg: Self::Config) -> Addr<Self::Processor> {
-        // Load TFLite model from the path in the config
-        let model = Model::from_file(cfg.model_path.as_str()).unwrap();
-
-        let int = Interpreter::new(model, self.dev).unwrap();
-
-        MlInterpreter { int }.start()
-    }
+    type Output = Tensor<Output, f32>;
+    type Error = Box<dyn std::error::Error + Send>;
 }
