1df18aa6676349a3c61ee856d13f471567de0e07
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2025-03-18 16:47:04 -0400

    bs

---
+++ ui/src/routes/+components/CameraFeed.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Camera } from '$lib/api';
+	import { client } from '$lib/api/client.gen';
+	import { Card } from 'flowbite-svelte';
+
+	let {
+		camera = $bindable(null),
+	}: { camera: Camera | null } = $props();
+</script>
+
+{#if camera}
+	<Card class="h-min" padding="xs">
+		<img src={`${client.getConfig().baseUrl ? client.getConfig().baseUrl : ''}/stream/${camera.id}`} alt={camera.name} />
+	</Card>
+{/if}
