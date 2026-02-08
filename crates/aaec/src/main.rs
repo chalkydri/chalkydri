90553b51e8148f69146aca028f7fb3bf5070dc5f
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2026-02-07 22:21:52 -0500

    start implementing AAEC

---
+++ crates/aaec/src/main.rs
@@ -0,0 +1,19 @@
+use nalgebra::matrix;
+use nalgebra::Matrix3;
+
+const SOBEL_KERNEL_DX: Matrix3<f64> = matrix![
+    -0.125, 0.0, 0.125;
+    -0.25, 0.0, 0.25;
+    -0.125, 0.0, 0.125;
+];
+const SOBEL_KERNEL_DY: Matrix3<f64> = matrix![
+    -0.125, -0.25, -0.125;
+    0.0, 0.0, 0.0;
+    0.125, 0.25, 0.125;
+];
+
+fn find_gradients() {
+}
+
+fn main() {
+}
